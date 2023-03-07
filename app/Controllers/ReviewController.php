d1975dacd8d985d61ccde1500a8311dbc095a437
AIGEOO <yosifsalimalessaei@gmail.com>
2023-03-07 08:40:40 +0400

    style(controllers): change controllers directory name to Controllers

---
+++ app/Controllers/ReviewController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\View;
+use Core\Request;
+use Core\Response;
+
+class ReviewController
+{
+    public function __construct (
+        public Request $request,
+        public Response $response
+    ) {}
+
+    public function index()
+    {
+        //
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function show()
+    {
+        //
+    }
+
+    public function edit()
+    {
+        //
+    }
+
+    public function update()
+    {
+        //
+    }
+
+    public function destroy()
+    {
+        //
+    }
+}
